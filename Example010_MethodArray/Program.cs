6d4bfc0c77c07b8b600f73cc4512f824a7d6df09
Юрий Мухаметзянов <YusufuMukhametzyanov@yandex.ru>
2022-10-04 09:03:07 +0500

    Add examples 8, 9, 10, 11

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = { 1, 2, 3, 65, 4, 65, 46, 27, 18 };
+
+int n = array.Length;
+int find = 65;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    // index = index + 1;
+    index++;
+}
